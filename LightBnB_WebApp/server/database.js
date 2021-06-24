0f57ae64f9883861e6e4b90fa3bcddfa41f3d414
Soliloquy <ibrahia@ucalgary.ca>
2021-06-24 22:29:43 +0000

    Modify getAllProperties function to include filters

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -110,16 +110,55 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+ const getAllProperties = function (options, limit = 10) {
+  // 1 Setup array to hold paramaters that may be available for query
+  const queryParams = [];
+  // 2 Start query with all information before filter (WHERE clause)
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1 = 1
+  `;
+  console.log(options)
+
+  // 3 Check if options exist, and add to query if so
+  if (options.city) {
+    queryParams.push(`%${options.city.toLowerCase()}%`);
+    queryString += ` AND lower(city) LIKE $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += ` AND cost_per_night > $${queryParams.length} `
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100)
+    queryString += ` AND cost_per_night < $${queryParams.length} `
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating)
+    queryString += ` AND rating > $${queryParams.length} `
+  }
+
+ 
+  // 4 Add queries that come after filter, that are not options
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
+
 
 exports.getAllProperties = getAllProperties;
 
